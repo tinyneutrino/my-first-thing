b9df029a09b08bac7aed99c52fcd25daa40057e8
NannaBill <nannabillandersen@gmail.com>
2016-08-30 13:52:46 +0200

    My Django app, first commit

---
+++ sublime_stuff/test.py
@@ -0,0 +1,29 @@
+# print('Goddag, Nanna')
+
+# if 3 > 5:
+# 	print('3 is greater than 5')
+
+# else:
+# 	print('3 is not greather than 5')
+
+# name='Sonja'
+
+# if name=='Nanna':
+# 	print('Hello, Nanna!')
+# elif name=='Thea':
+# 	print ('Hello, Thea!')
+# else:
+# 	print ('Hello, anonymous!') 
+
+def hi(name):
+	print ('Hi, '+name)
+	print ('How are you?')
+
+
+hi('Nanna')
+
+girls=['Nanna','Thea','Emma','Slagathor']
+
+for name in girls:
+	hi(name)
+	print('Next, please')
